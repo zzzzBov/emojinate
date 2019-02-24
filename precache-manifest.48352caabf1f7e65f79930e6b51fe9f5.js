8a09dfa245b27715c9a1aee34f934b81cfc81c6e
Tim Leverett <zzzzbov@gmail.com>
2019-02-24 18:15:01 -0500

    Updates

---
+++ precache-manifest.48352caabf1f7e65f79930e6b51fe9f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5889cd5241dd997c4649",
+    "url": "/emojinate/static/js/runtime~main.5889cd52.js"
+  },
+  {
+    "revision": "24712af33ad65b3a3238",
+    "url": "/emojinate/static/js/main.24712af3.chunk.js"
+  },
+  {
+    "revision": "f0001dffdea2202ad7e2",
+    "url": "/emojinate/static/js/1.f0001dff.chunk.js"
+  },
+  {
+    "revision": "24712af33ad65b3a3238",
+    "url": "/emojinate/static/css/main.e2c584aa.chunk.css"
+  },
+  {
+    "revision": "885b7c9ddca150a1c5a4b56c4e95cfc9",
+    "url": "/emojinate/index.html"
+  }
+];
